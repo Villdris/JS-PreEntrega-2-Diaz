98c0ef0e5e7260afc3edb7f1cebd58cad72ab480
oscarDiaz <oskar.d.z@hotmail.com>
2024-07-20 23:08:34 -0400

    Botones agregados para la version movile, y error solucionado

--- js/calculos.js
+++ js/calculos.js
@@ -107,7 +107,7 @@ function registrarUsuario() {
         setTimeout(function() {           //Mi Segunda Asincronia :DD
             Swal.fire({                   //Alerta copiar y pegar desde la documentacion :P
                 title: "¿Como Jugar?",
-                text: "Al escribir la respuesta puedes presionar la tecla ENTER o la Barra Espaciadora",
+                text: "Al escribir la respuesta puedes presionar la tecla ENTER || El boton de la derecha (si aparece)",
                 icon: "info"
                 }).then((result) => {
                     if (result.isConfirmed) {
@@ -134,82 +134,114 @@ function actualizarDatosUsuario(tipo) {
     setData()
 }
 
+let domSumasCajas = document.getElementsByClassName('sumas_caja');
+let domRestasCajas = document.getElementsByClassName('resta_caja');
+let domMultiplicacionesCajas = document.getElementsByClassName('multiplicacion_caja');
+let domDivisionesCajas = document.getElementsByClassName('division_caja');
 
 function crearNumerosAleatorios() {
-    
-    let domSumasCajas = document.getElementsByClassName('sumas_caja');
-    
     for (let i = 0; i < domSumasCajas.length; i++) {
         let domNumUno = domSumasCajas[i].getElementsByClassName('sumas_caja_numero')[0];
         let domNumDos = domSumasCajas[i].getElementsByClassName('sumas_caja_numeroDos')[0];
         
         let inputDomRespuesta = domSumasCajas[i].getElementsByClassName('respuesta')[0];
         
+        //agregando botones para version mobile o movile?                                     |
+        //antes habia agregado la tecla 'espacio' y funcionaba en pc, pero en mobile no       | 
+        //asi que, hoy me dedicare a aplicar botones :D                                       |
+        let sumasCajaBoton = domSumasCajas[i].getElementsByClassName('sumas_caja_boton')[0];//|
+        
         domNumUno.innerText = Math.floor(Math.random() * 10) + 1;
         domNumDos.innerText = Math.floor(Math.random() * 10) + 1;
         
         inputDomRespuesta.addEventListener('keypress', function (e) {
-            if (e.key === 'Enter' || e.key === ' ') {
+            if (e.key === 'Enter') {
                 comprobarRespuesta(domNumUno, domNumDos, inputDomRespuesta, 'suma');
             }
         });
+
+        sumasCajaBoton.addEventListener('click', function () {
+            comprobarRespuesta(domNumUno, domNumDos, inputDomRespuesta, 'suma');
+            sumasCajaBoton.classList.remove('solo_moviles');
+            sumasCajaBoton.classList.add('ocultar');
+        });
     }
 
-    let domRestasCajas = document.getElementsByClassName('resta_caja');
     
     for (let i = 0; i < domRestasCajas.length; i++) {
         let domNumUno = domRestasCajas[i].getElementsByClassName('resta_caja_numero')[0];
         let domNumDos = domRestasCajas[i].getElementsByClassName('resta_caja_numeroDos')[0];
         
         let inputDomRespuesta = domRestasCajas[i].getElementsByClassName('respuesta')[0];
+
+        let restaCajaBoton = domRestasCajas[i].getElementsByClassName('resta_caja_boton')[0]; //boton movile
         
         domNumUno.innerText = Math.floor(Math.random() * (10 - 5 + 1)) + 5;
         domNumDos.innerText = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
-        
+
         inputDomRespuesta.addEventListener('keypress', function (e) {
-            if (e.key === 'Enter' || e.key === ' ') {
+            if (e.key === 'Enter') {
                 comprobarRespuesta(domNumUno, domNumDos, inputDomRespuesta, 'resta');
             }
         });
+
+        restaCajaBoton.addEventListener('click', function () { // funcion boton mobile
+            comprobarRespuesta(domNumUno, domNumDos, inputDomRespuesta, 'resta');
+            restaCajaBoton.classList.remove('solo_moviles');
+            restaCajaBoton.classList.add('ocultar');
+        });
     }
 
-    let domMultiplicacionesCajas = document.getElementsByClassName('multiplicacion_caja');
     
     for (let i = 0; i < domMultiplicacionesCajas.length; i++) {
         let domNumUno = domMultiplicacionesCajas[i].getElementsByClassName('multiplicacion_caja_numero')[0];
         let domNumDos = domMultiplicacionesCajas[i].getElementsByClassName('multiplicacion_caja_numeroDos')[0];
         
         let inputDomRespuesta = domMultiplicacionesCajas[i].getElementsByClassName('respuesta')[0];
+
+        let multiplicacionCajaBoton = domMultiplicacionesCajas[i].getElementsByClassName('multiplicacion_caja_boton')[0]; //boton moBile
         
         domNumUno.innerText = Math.floor(Math.random() *(5 - 1 + 1)) + 1;
         domNumDos.innerText = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
         
         inputDomRespuesta.addEventListener('keypress', function (e) {
-            if (e.key === 'Enter' || e.key === ' ') {
+            if (e.key === 'Enter') {
                 comprobarRespuesta(domNumUno, domNumDos, inputDomRespuesta, 'multiplicacion');
             }
+        });
+
+        multiplicacionCajaBoton.addEventListener('click', function () { // funcion boton mobile
+            comprobarRespuesta(domNumUno, domNumDos, inputDomRespuesta, 'multiplicacion');
+            multiplicacionCajaBoton.classList.remove('solo_moviles')
+            multiplicacionCajaBoton.classList.add('ocultar');
         });
     }
 
 
-    let domDivisionesCajas = document.getElementsByClassName('division_caja');
     
     for (let i = 0; i < domDivisionesCajas.length; i++) {
         let domNumUno = domDivisionesCajas[i].getElementsByClassName('division_caja_numero')[0];
         let domNumDos = domDivisionesCajas[i].getElementsByClassName('division_caja_numeroDos')[0];
         
         let inputDomRespuesta = domDivisionesCajas[i].getElementsByClassName('respuesta')[0];
+
+        let divisionCajaBoton = domDivisionesCajas[i].getElementsByClassName('division_caja_boton')[0];
         
         domNumUno.innerText = Math.floor(Math.random() * (10 - 5 + 1)) + 5;
         domNumDos.innerText = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
         
         inputDomRespuesta.addEventListener('keypress', function (e) {  
-            if (e.key === ' ' || e.key === 'Enter') {
+            if (e.key === 'Enter') {
                 comprobarRespuesta(domNumUno, domNumDos, inputDomRespuesta, 'division');
             }
         });
-    }
-}
+
+        divisionCajaBoton.addEventListener('click', function () { // funcion boton mobvile
+            comprobarRespuesta(domNumUno, domNumDos, inputDomRespuesta, 'division');
+            divisionCajaBoton.classList.remove('solo_moviles');
+            divisionCajaBoton.classList.add('ocultar');
+        });
+    }}
 crearNumerosAleatorios()
 
 
@@ -333,8 +365,8 @@ function reset(){
             actualizarDatosUsuario('multiplicacion');
             actualizarDatosUsuario('division');
 
-            usuario.registros= [];
             limpiarInputs()
+            clonesDeNodos()
             crearNumerosAleatorios()
             setData();
 
@@ -344,6 +376,25 @@ function reset(){
             let allContent = document.body.children;
             for (let i = 0; i < allContent.length; i++) {
                 allContent[i].style.display = '';
+            }
+
+            for (let i = 0; i < domSumasCajas.length; i++) {//volver a mostrar los botones, hice cambios mas arriba
+                                                            //para no repetir lineas, Las cajas las hice globales.
+                                                            // Cambios sin errores.
+
+                let sumasCajaBoton = domSumasCajas[i].getElementsByClassName('sumas_caja_boton')[0];
+                let restaCajaBoton = domRestasCajas[i].getElementsByClassName('resta_caja_boton')[0];
+                let multipliCajaBoton = domMultiplicacionesCajas[i].getElementsByClassName('multiplicacion_caja_boton')[0];
+                let divisionCajaBoton = domDivisionesCajas[i].getElementsByClassName('division_caja_boton')[0];
+
+                sumasCajaBoton.classList.remove('ocultar');
+                sumasCajaBoton.classList.add('solo_moviles');
+                restaCajaBoton.classList.remove('ocultar');
+                restaCajaBoton.classList.add('solo_moviles');
+                multipliCajaBoton.classList.remove('ocultar');
+                multipliCajaBoton.classList.add('solo_moviles');
+                divisionCajaBoton.classList.remove('ocultar');
+                divisionCajaBoton.classList.add('solo_moviles')
             }
         }
     else{
@@ -418,4 +469,42 @@ function mostrarEjerciciosYOcultarRegistro() {
         cajaRegistroDom.classList.add('ocultar');
         mostrarTabla();
     }, 2000);
+}
+// dije que dejaria los parches aqui abajo, pero algunos no se pueden: "los botones en movile"
+// ya busque y es *Movile*, no _mobile_.
+
+// Existia un error muy grave desde el primer momento en que implemente el Boton RESET
+// Desde la 3ra-Entrega y esa es:
+/* El oprimir Reset, jugar y apretar el boton o hacer "Enter", Quitaba Dos Intentos en lugar de Uno
+    
+    Depurando el codigo vi que la funcion comprobarRespuesta() se ejecutaba dos veces solamente
+        despues del RESET. Despues de varios dias con esa Vill-Sombra sobre mi
+        un dia cualquiera pensé, al ir a la pagina se agrega el evento Click && Enter ok todo bien
+        al Resetear los agrega denuevo cierto, CLARO, desde allí comenze a jalar la cuerda.
+            Primer parche fallido: antes de la linea que agrega los evento, puse un removeEvenListener
+                sin exito, el error seguia (No eran los eventos xD o eso creia).
+            Luego de varios cambios y nuevos errores para reparar ese Vill Error
+
+            No queria seguir en esto y Le pregunte a chatGPT
+            entre todas las soluciones que me dio, esta resulto mas facil de entender
+
+            clonesDeNodos() Entiendo lo que hace: toma todos los input y botones
+                reemplaza los mios por los Clones que no tienen eventos :D
+                Claro, despues se agregan nuevamente una vez
+                entiendo que al presionar Reset siempre hará clones y nunca habra eventos duplicados.
+
+    anteriormente agrege un metodo que actualiza la pagina, pero
+    encontre que esa solucion era muy ordinaria y la descarté xD
+
+    Perdón por los comentarios, siento que tenia que documentar esto, dias y horas en un solo ERROR
+    arreglado en 10 lineas -maravilloso- (aun no sé que API usar);
+*/  
+function clonesDeNodos() {
+    let cajas = document.querySelectorAll('.respuesta, .sumas_caja_boton, .resta_caja_boton, .multiplicacion_caja_boton, .division_caja_boton');
+    
+    cajas.forEach(caja => {
+        
+        let clon = caja.cloneNode(true);
+        caja.parentNode.replaceChild(clon, caja);
+    });
 }
